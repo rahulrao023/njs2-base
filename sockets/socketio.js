7fb6ae2bec8119957804a29f2721553edd2ac9b7
rahul.rao <rahul.rao@juegostudioz.com>
2024-07-05 12:26:27 +0530

    Added polling fallback in socket.io for connection stability

--- sockets/socketio.js
+++ sockets/socketio.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const io = require("socket.io")({
   serveClient: false,
-  transports: ["websocket"],
+  transports: ["websocket", "polling"],
   cors: {
     origin: "*",
   },
